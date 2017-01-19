39f35f38e4628fc0da1c75045e2ac4e1c5e1b6a1
andigan <andrew.nease.code@gmail.com>
2017-01-19 15:33:09 -0500

    working draft

--- index.js
+++ index.js
@@ -1,88 +1,162 @@
+$( document ).ready(function() {
 
-// creates a new XMLHTTPRequest
-// open to get it started
-// send to send it
+  // creates a new XMLHTTPRequest
+  // open to get it started
+  // send to send it
 
-// function getRepositories() {
-//   const req = new XMLHttpRequest()
-//   req.open("GET", 'https://api.github.com/users/andigan/repos')
-//   req.send()
-// }
+  // function getRepositories() {
+  //   const req = new XMLHttpRequest()
+  //   req.open("GET", 'https://api.github.com/users/andigan/repos')
+  //   req.send()
+  // }
 
+  $('#artist_search_form').submit(function (event) {
 
-function getRepositories() {
-  const req = new XMLHttpRequest()
+    // prevent page reload
+    event.preventDefault();
 
-  // now we're adding an eventlistener to the request
-  // that fires when data is loaded and executes
-  // the callback
-  req.addEventListener("load", showRepositories);
-  // what does true do?
-//  req.open("GET", 'https://api.github.com/users/andigan/repos', true)
-  req.open("GET", 'https://www.googleapis.com/books/v1/volumes?q=ruby+programming')
+//    alert($('#artist_input').val());
 
-  req.send()
+    searchArtistRequest();
+  });
 
+//document.getElementById('name_input').value;
 
-  // other methods
+// https://api.spotify.com/v1/search?type=artist&q=michael+jackson
+  function searchArtistRequest () {
+    var input = document.getElementById('artist_input').value
+    console.log(input)
+    let query = input.split(' ').join('+')
 
-  // this will point to req
-  req.onload = function () {
-    let data = JSON.parse (this.response);
-    console.log(data);
+    const req = new XMLHttpRequest()
+    req.addEventListener("load", showArtists);
+    req.open('GET', `https://api.spotify.com/v1/search?type=artist&q=${query}`);
+    req.send();
+  }
+
+  function showArtists() {
+      var artists_list = document.getElementById("artists_list")
+      artists_list.innerHTML = ""
+      temp_response = JSON.parse(this.responseText)
+      temp_response['artists']['items'].forEach(function(item, index) {
+      //  console.log(item['name'], item['id'])
+
+      var list_item_element = document.createElement("li")
+      list_item_element.textContent = item['name']
+      artists_list.appendChild(list_item_element)
+     })
 
   }
 
 
-}
-
-// the callback:
-// how does this work?
-// does addEventListener have a consistent expectation
-// for it's callback parameters?
-
-// function showRepositories(event, data) {
-//   //this is set to the XMLHttpRequest object that fired the event
-//   // this equals req object
-//   // which has a responseText once it is loaded up somehow
-//   console.log(this.responseText)
-//   let repoList = "<ul>"
-//   for(var i=0;i < this.responseText.length; i++) {
-//     repoList += "<li>" + this.responseText[i]["name"] + "</li>"
-//   }
-//   repoList += "</ul>"
-//   document.getElementById("repositories").innerHTML = repoList
-// }
 
 
-// function showRepositories(event, data) {
-// //  parse the response data into json
-//   var repos = JSON.parse(this.responseText)
-//   console.log(repos)
-//
-//
-//   const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
-//   document.getElementById("repositories").innerHTML = repoList
-// }
 
 
-function showRepositories(event, data) {
-  var repos = JSON.parse(this.responseText)
-  console.log(repos)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
-  document.getElementById("repositories").innerHTML = repoList
-}
 
-function getCommits(el) {
-  const name = el.dataset.repo
-  const req = new XMLHttpRequest()
-  req.addEventListener("load", showCommits);
-  req.open("GET", 'https://api.github.com/repos/andigan/' + name + '/commits')
-  req.send()
-}
 
-function showCommits() {
-  const commits = JSON.parse(this.responseText)
-  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
-  document.getElementById("commits").innerHTML = commitsList
-}
+
+
+
+
+
+
+
+
+
+
+  //
+  // function getRepositories() {
+  //   const req = new XMLHttpRequest()
+  //
+  //   // now we're adding an eventlistener to the request
+  //   // that fires when data is loaded and executes
+  //   // the callback
+  //   req.addEventListener("load", showRepositories);
+  //   // what does true do?
+  // //  req.open("GET", 'https://api.github.com/users/andigan/repos', true)
+  //   req.open("GET", 'https://www.googleapis.com/books/v1/volumes?q=ruby+programming')
+  //
+  //   req.send()
+  //
+  //
+  //   // other methods
+  //
+  //   // this will point to req
+  //   req.onload = function () {
+  //     let data = JSON.parse (this.response);
+  //     console.log(data);
+  //
+  //   }
+  //
+  //
+  // }
+  //
+  // // the callback:
+  // // how does this work?
+  // // does addEventListener have a consistent expectation
+  // // for it's callback parameters?
+  //
+  // // function showRepositories(event, data) {
+  // //   //this is set to the XMLHttpRequest object that fired the event
+  // //   // this equals req object
+  // //   // which has a responseText once it is loaded up somehow
+  // //   console.log(this.responseText)
+  // //   let repoList = "<ul>"
+  // //   for(var i=0;i < this.responseText.length; i++) {
+  // //     repoList += "<li>" + this.responseText[i]["name"] + "</li>"
+  // //   }
+  // //   repoList += "</ul>"
+  // //   document.getElementById("repositories").innerHTML = repoList
+  // // }
+  //
+  //
+  // // function showRepositories(event, data) {
+  // // //  parse the response data into json
+  // //   var repos = JSON.parse(this.responseText)
+  // //   console.log(repos)
+  // //
+  // //
+  // //   const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
+  // //   document.getElementById("repositories").innerHTML = repoList
+  // // }
+  //
+  //
+  // function showRepositories(event, data) {
+  //   var repos = JSON.parse(this.responseText)
+  //   console.log(repos)
+  //   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  //   document.getElementById("repositories").innerHTML = repoList
+  // }
+  //
+  // function getCommits(el) {
+  //   const name = el.dataset.repo
+  //   const req = new XMLHttpRequest()
+  //   req.addEventListener("load", showCommits);
+  //   req.open("GET", 'https://api.github.com/repos/andigan/' + name + '/commits')
+  //   req.send()
+  // }
+  //
+  // function showCommits() {
+  //   const commits = JSON.parse(this.responseText)
+  //   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  //   document.getElementById("commits").innerHTML = commitsList
+  // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
