952072db159d80824e50dc5f2b047010da19fd12
andigan <andrew.nease.code@gmail.com>
2017-01-19 17:56:14 -0500

    second working copy

--- index.js
+++ index.js
@@ -1,55 +1,89 @@
 $( document ).ready(function() {
 
-  // creates a new XMLHTTPRequest
-  // open to get it started
-  // send to send it
-
-  // function getRepositories() {
-  //   const req = new XMLHttpRequest()
-  //   req.open("GET", 'https://api.github.com/users/andigan/repos')
-  //   req.send()
-  // }
 
   $('#artist_search_form').submit(function (event) {
-
     // prevent page reload
     event.preventDefault();
-
-//    alert($('#artist_input').val());
 
     searchArtistRequest();
   });
 
-//document.getElementById('name_input').value;
 
-// https://api.spotify.com/v1/search?type=artist&q=michael+jackson
+
+  // Example api url: https://api.spotify.com/v1/search?type=artist&q=michael+jackson
   function searchArtistRequest () {
+    // get input string from form
     var input = document.getElementById('artist_input').value
-    console.log(input)
+    // prepare the query string
     let query = input.split(' ').join('+')
 
     const req = new XMLHttpRequest()
+
     req.addEventListener("load", showArtists);
     req.open('GET', `https://api.spotify.com/v1/search?type=artist&q=${query}`);
     req.send();
   }
 
+
+
+  // var a = document.createElement('a');
+  // var linkText = document.createTextNode("my title text");
+  // a.appendChild(linkText);
+  // a.title = "my title text";
+  // a.href = "http://example.com";
+  // document.body.appendChild(a)
+
+
+
   function showArtists() {
+    // var list_item_element = document.createElement("li")
       var artists_list = document.getElementById("artists_list")
       artists_list.innerHTML = ""
       temp_response = JSON.parse(this.responseText)
       temp_response['artists']['items'].forEach(function(item, index) {
       //  console.log(item['name'], item['id'])
 
-      var list_item_element = document.createElement("li")
-      list_item_element.textContent = item['name']
-      artists_list.appendChild(list_item_element)
-     })
+      var a_element = document.createElement('a')
+      var link_text = document.createTextNode(item['name'])
+      a_element.appendChild(link_text)
+      a_element.title = item['name']
+      a_element.href = '#'
+      a_element.setAttribute('id', item['id'])
+      a_element.classList.add('artist_link')
+      artists_list.appendChild(a_element)
 
+
+
+      // list_item_element.textContent = item['name']
+      // artists_list.appendChild(list_item_element)
+     })
   }
 
 
+  $(document).on("click",".artist_link",function() {
 
+      var id = this.getAttribute('id')
+      $.ajax({
+        url: `https://api.spotify.com/v1/artists/${id}/albums`,
+        success: renderAlbums
+      })
+
+
+
+  });
+
+  function renderAlbums(data) {
+    //data['items'][0]['name']
+    let $albumList = $('#albums_list')
+    $albumList.html('')
+
+    data['items'].forEach(function(album){
+      //console.log(album['name'])
+      $albumList.append(`<li class='album_name'>${album['name']}</li>`)
+
+
+    })
+  }
 
 
 
